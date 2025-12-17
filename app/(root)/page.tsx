c97f43fb4df3cff84b3c9f5f19ac2ee42aa33041
Farid Ganbarov <17003685+faridnextjs@users.noreply.github.com>
2025-12-18 00:05:30 +0100

    Nested Routes, Dynamic Route Segment [slug]

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,11 @@
 import TestClient from "@/components/testClient";
 
 export default function Home() {
-    console.log("I am Server Component");
-    return (
-        <main>
- Main   Page
-            <TestClient />
-        </main>
-    );
+  console.log("I am Server Component");
+  return (
+    <main>
+      Main Page
+      <TestClient />
+    </main>
+  );
 }
