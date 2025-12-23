9019d1e409c55d7777a5998f96390095a514d13a
Farid Ganbarov <17003685+faridnextjs@users.noreply.github.com>
2025-12-23 01:04:49 +0100

    added tailwind css + shadcn ui

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,11 +3,9 @@ import TestClient from "@/components/testClient";
 export default function Home() {
   console.log("I am Server Component");
   return (
-    <main>
-      <h1 className="text-light-500 text-2xl">
-        Welcome to the Next.js 16 (Stable) Showdown
-      </h1>
+    <>
+      <h1>Welcome to the Ultimate Next.js 16 (Stable) Showdown</h1>
       <TestClient />
-    </main>
+    </>
   );
 }
--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    reactCompiler: true,
-    cacheComponents: true,
-    crossOrigin: 'anonymous',
+  reactCompiler: true,
+  cacheComponents: true,
+  crossOrigin: "anonymous",
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
